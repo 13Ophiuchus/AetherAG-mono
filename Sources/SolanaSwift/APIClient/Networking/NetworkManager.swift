2bd5b0f66aa9c403634c7f1549f5162efa9fd2a7
Nic Reich <ophiuchus@resourceless.org>
2026-08-04 18:40:02 -0400

    Revert "fix(security): use Base64URL.decode() for JWK x/y parsing in ES256JWSVerifier"
    This reverts commit 777b010da16264c9adcc0d1f96b31d6ba50fd60d.

--- Sources/SolanaSwift/APIClient/Networking/NetworkManager.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-#if canImport(FoundationNetworking)
-import FoundationNetworking
-#endif
-
-public protocol NetworkManager: Sendable {
-    func requestData(request: URLRequest) async throws -> Data
-}
--- Sources/SolanaSwift/BlockchainClient/Helpers/CryptoSwift/SHA256.swift
+++
@@ -1,7 +0,0 @@
-import Crypto
-import Foundation
-
-public func sha256(data: Data) -> Data {
-    let digest = SHA256.hash(data: data)
-    return Data(digest)
-}
